15bf8604c04b36dabe914021685302c7a6e687f5
asmagala <asmagala@op.pl>
2020-07-28 12:25:13 +0200

    Remove comments.

--- server.js
+++ server.js
@@ -10,23 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, `/client/index.html`));
 });
 
-/*
-const db = require('./data/db.js');
-const testimonialsRoutes = require('./routes/testimonials.routes');
-const concertsRoutes = require('./routes/concerts.routes');
-const seatsRoutes = require('./routes/seats.routes');
-app.use('/api', testimonialsRoutes);
-app.use('/api', concertsRoutes);
-app.use('/api', seatsRoutes);
-
-//app.use(express.urlencoded({extended: false}));
-//app.use(express.json());
-
-app.use((req, res) => {
-  res.status(404).json({ "message": "Not found..."});
-});
-*/
-
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
